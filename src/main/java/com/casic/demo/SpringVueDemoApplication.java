a8ede466a04b5ed29ebbee4e2a033940c88e3a63
xuli <78645574@qq.com>
2018-06-07 18:03:48 +0800

    格式化

--- src/main/java/com/casic/demo/SpringVueDemoApplication.java
+++ src/main/java/com/casic/demo/SpringVueDemoApplication.java
@@ -1,5 +1,8 @@
 package com.casic.demo;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
@@ -9,18 +12,16 @@ import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class SpringVueDemoApplication {
+    public static void main(String[] args) throws IOException {
+        Desktop.getDesktop().browse(URI.create("http://localhost:8888/"));
+        SpringApplication.run(SpringVueDemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringVueDemoApplication.class, args);
-	}
-
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-
-		return (container -> {
-			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-
-			container.addErrorPages(error404Page);
-		});
-	}
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            container.addErrorPages(error404Page);
+        });
+    }
 }
